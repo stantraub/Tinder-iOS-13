f11a0cd2fb8e7724992cefa2f53ff98d59f4e920
Stanley <stanley.traub@gmail.com>
2020-07-09 19:27:58 -0700

    Initial commit

---
+++ Tinder iOS 13/View/CardView.swift
@@ -0,0 +1,147 @@
+//
+//  CardView.swift
+//  Tinder iOS 13
+//
+//  Created by Stanley Traub on 7/9/20.
+//  Copyright © 2020 Stanley Traub. All rights reserved.
+//
+
+import UIKit
+
+enum SwipeDirection: Int {
+    case left = -1
+    case right = 1
+}
+
+class CardView: UIView {
+    //MARK: - Properties
+    
+    private let gradientLayer = CAGradientLayer()
+    
+    private let imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.image = #imageLiteral(resourceName: "kelly1")
+        return iv
+    }()
+    
+    private let infoLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 2
+        
+        let attributedText = NSMutableAttributedString(string: "Jane Doe",
+                                                       attributes: [.font: UIFont.systemFont(ofSize: 32, weight: .heavy), .foregroundColor: UIColor.white])
+        
+        attributedText.append(NSAttributedString(string: "  20", attributes: [.font:
+            UIFont.systemFont(ofSize: 24), .foregroundColor: UIColor.white]))
+        
+        label.attributedText = attributedText
+        
+        return label
+    }()
+    
+    private lazy var infoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "info_icon").withRenderingMode(.alwaysOriginal), for: .normal)
+        return button
+    }()
+    
+    //MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureGestureRecognizers()
+
+        backgroundColor = .systemPurple
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        
+        addSubview(imageView)
+        imageView.fillSuperview()
+        
+        configureGradientLayer()
+        
+        addSubview(infoLabel)
+        infoLabel.anchor(left: leftAnchor, bottom: bottomAnchor, right: rightAnchor,
+                         paddingLeft: 16, paddingBottom: 16, paddingRight: 16)
+        
+        addSubview(infoButton)
+        infoButton.setDimensions(height: 40, width: 40)
+        infoButton.centerY(inView: infoLabel)
+        infoButton.anchor(right: rightAnchor, paddingRight: 16)
+        
+    }
+    
+    override func layoutSubviews() {
+        gradientLayer.frame = self.frame
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Selectors
+    
+    @objc func handlePanGesture(sender: UIPanGestureRecognizer) {
+        
+        switch sender.state {
+            case .began:
+                //makes sure swiping will remain smooth
+                superview?.subviews.forEach({$0.layer.removeAllAnimations()})
+            case .changed:
+                panCard(sender: sender)
+            case .ended:
+                resetCardPosition(sender: sender)
+            default: break
+        }
+    }
+    
+    @objc func handleChangePhoto(sender: UITapGestureRecognizer) {
+        print("uo")
+    }
+    
+    //MARK: - Helpers
+    
+    func panCard(sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: nil)
+        let degrees: CGFloat = translation.x / 20
+        let angle = degrees * .pi / 180
+        let rotationalTransform = CGAffineTransform(rotationAngle: angle)
+        self.transform = rotationalTransform.translatedBy(x: translation.x, y: translation.y)
+    }
+        
+    func resetCardPosition(sender: UIPanGestureRecognizer) {
+        let direction: SwipeDirection = sender.translation(in: nil).x > 100 ? .right : .left
+        let shouldDismissCard = abs(sender.translation(in: nil).x) > 100
+        UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: .curveEaseOut, animations: {
+            
+            if shouldDismissCard {
+                let xTranslation = CGFloat(direction.rawValue) * 1000
+                let offScreenTransform = self.transform.translatedBy(x: xTranslation, y: 0)
+                self.transform = offScreenTransform
+            } else {
+                self.transform = .identity
+            }
+            
+            self.transform = .identity
+        }) { _ in
+            if shouldDismissCard {
+                self.removeFromSuperview()
+            }
+        }
+    }
+    
+    func configureGradientLayer() {
+        gradientLayer.colors = [UIColor.clear.cgColor, UIColor.black.cgColor]
+        gradientLayer.locations = [0.5, 1.1]
+        layer.addSublayer(gradientLayer)
+    }
+    
+    func configureGestureRecognizers() {
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(handlePanGesture))
+        addGestureRecognizer(pan)
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleChangePhoto))
+        addGestureRecognizer(tap)
+    }
+}
